6a4c818ed2326f07379391c21bc4e9dedd39e9ab
Villon CHEN <villon2chen@gmail.com>
2024-04-16 18:29:24 +0200

    Debugging

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ const app = createApp(App)
 app.config.globalProperties.USER = ref(undefined)
 
 // Debug only...
-// import users from './data/users.json'
-// app.config.globalProperties.USER.value = users[0]
+import users from './data/users.json'
+app.config.globalProperties.USER.value = users[0]
 
 // Components
 app.component('NavBarComponent', NavBarComponent)
