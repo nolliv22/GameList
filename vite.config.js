fb1cd21d72d1a5f5a8ec4ebf2597ca58baffe041
Villon CHEN <villon2chen@gmail.com>
2024-04-17 11:51:05 +0200

    Implement pwa using vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'GameList',
+        short_name: 'GameList',
+        theme_color: '#04042d',
+        display: "fullscreen",
+      },
+    }),
   ],
   resolve: {
     alias: {
